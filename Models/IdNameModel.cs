d0c8523def08b6c694ad7568f7b43d7dc2e9dd7f
simran-capsitech <simran.sharma@capsitech.com>
2024-07-19 11:39:06 +0530

    intial commit

---
+++ Models/IdNameModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ToDoList.Models
+{
+    public class IdModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+    public class IdNameModel : IdModel
+    {
+        [BsonIgnoreIfDefault,BsonIgnoreIfNull]
+        public string Name { get; set; }
+    }
+}
