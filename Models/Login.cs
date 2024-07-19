5efded8297b3d5c30b073cc49a925176cb27d819
simran-capsitech <simran.sharma@capsitech.com>
2024-07-19 12:21:07 +0530

    initial commit

---
+++ Models/Login.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ToDoList.Models
+{
+    public class Login : IdNameModel
+    {
+        [BsonElement("P"),BsonIgnoreIfNull]
+        public string PassWord { get; set; }
+    }
+}
